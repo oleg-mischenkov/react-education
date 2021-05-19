4dd2d6ffe2ee04b38eb48dd91d6ffeb65481a700
Oleh Mishchenkov <oleh_mishchenkov@epam.com>
2021-05-19 12:53:15 +0300

    each post as the array element

--- src/components/Profile/Posts/Post/Post.js
+++ src/components/Profile/Posts/Post/Post.js
@@ -5,6 +5,9 @@ const Post = (props)=> {
         <div className={css.item}>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq_I0JFO2DxoAV3J-sI7ajtx0qW0Q5neaY_A&usqp=CAU" alt="ava"/>
             <span>{props.msg}</span>
+            <div>
+                <span>likes: {props.likeCount}</span>
+            </div>
         </div>
     );
 }
--- src/components/Profile/Posts/Posts.js
+++ src/components/Profile/Posts/Posts.js
@@ -1,7 +1,13 @@
 import css from './Posts.module.css';
 import Post from "./Post/Post";
 
-const Posts = ()=> {
+const Posts = (props)=> {
+    let postData = [
+        {id: 1, msg: 'Hi, how are you?', likes: 7},
+        {id: 2, msg: 'It is my first post!', likes: 12},
+        {id: 3, msg: 'I am happy', likes: 8},
+    ];
+
     return (
         <div>
             <h4>My posts</h4>
@@ -11,9 +17,9 @@ const Posts = ()=> {
             <div>
                 <button>add</button>
             </div>
-            <Post msg='Hi, how are you?' />
-            <Post msg='It is my first post!'/>
-            <Post msg='I am happy/'/>
+            <Post msg={postData[0].msg} likeCount={postData[0].likes} />
+            <Post msg={postData[1].msg} likeCount={postData[1].likes} />
+            <Post msg={postData[2].msg} likeCount={postData[2].likes} />
         </div>
     );
 }
