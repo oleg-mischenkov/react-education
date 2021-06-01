8a86262f852dd72a9562057a7c04274847e2c707
Oleh Mishchenkov <oleh_mishchenkov@epam.com>
2021-06-01 22:50:56 +0300

    add user class component

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,64 +1,45 @@
 import React from "react";
 import css from './Users.module.css'
 import User from "./User/User";
-import * as axios from "axios";
 
-class Users extends React.Component {
+let Users = (props) => {
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.usersOnPage}&page=${this.props.currentPage}`)
-            .then( resp => {
-                this.props.sendUsers(resp.data.items);
-                this.props.sendTotalUserCount(resp.data.totalCount);
-            } );
+    let finPoint = Math.ceil( props.totalUserCount / props.usersOnPage );
+    let midlPoint = props.maxPaginationButtonOnPage / 2;
+
+    let preStart = props.currentPage - midlPoint;
+    let startPoint = preStart < 1 ? 1 : preStart;
+
+    let preEnd = props.currentPage + midlPoint;
+    let endPoint = preEnd > finPoint ? finPoint : preEnd;
+
+    let pages = [];
+    for (let i = startPoint; i <= endPoint; i++) {
+        pages.push(i);
     }
 
-    changePage = page => {
-        this.props.sendCurrentPage(page);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.usersOnPage}&page=${page}`)
-            .then( resp => {
-                this.props.sendUsers(resp.data.items);
-            } );
-    }
-
-    render() {
-        let finPoint = Math.ceil( this.props.totalUserCount / this.props.usersOnPage );
-        let midlPoint = this.props.maxPaginationButtonOnPage / 2;
-
-        let preStart = this.props.currentPage - midlPoint;
-        let startPoint = preStart < 1 ? 1 : preStart;
-
-        let preEnd = this.props.currentPage + midlPoint;
-        let endPoint = preEnd > finPoint ? finPoint : preEnd;
-
-        let pages = [];
-        for (let i = startPoint; i <= endPoint; i++) {
-            pages.push(i);
-        }
-
-        return (
-            <div>
-                <div className={css.paginationBlock}>
-                    {pages.map( page => {
-                        return <span
-                            className={ this.props.currentPage === page ? css.selectedPage: ''}
-                            onClick={(e) => this.changePage(page) }>
+    return (
+        <div>
+            <div className={css.paginationBlock}>
+                {pages.map( page => {
+                    return <span
+                        className={ props.currentPage === page ? css.selectedPage: ''}
+                        onClick={(e) => props.changePage(page) }>
                             {page}
                         </span>
-                    } )}
-                </div>
-                {this.props.users.map( user => <User
-                    id={user.id}
-                    avatar={user.photos.small}
-                    name={user.name}
-                    followed={user.followed}
-                    status={user.status}
-                    changeFollow={ id => this.props.follow(id) }
-                    changeUnfollow={ id => this.props.unfollow(id) }
-                /> )}
+                } )}
             </div>
-        );
-    }
+            {props.users.map( user => <User
+                id={user.id}
+                avatar={user.photos.small}
+                name={user.name}
+                followed={user.followed}
+                status={user.status}
+                changeFollow={ id => props.follow(id) }
+                changeUnfollow={ id => props.unfollow(id) }
+            /> )}
+        </div>
+    );
 }
 
 export default Users;
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,11 +1,46 @@
 import {connect} from "react-redux";
 import Users from './Users'
 import {
-    followActionType, sendCurrentPageActionType,
+    followActionType,
+    sendCurrentPageActionType,
     sendTotalUserCountActionType,
     sendUsersActionType,
     unfollowActionType
 } from "../../redux/reducer/users-reducer";
+import React from "react";
+import * as axios from "axios";
+
+class UsersComponent extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.usersOnPage}&page=${this.props.currentPage}`)
+            .then( resp => {
+                this.props.sendUsers(resp.data.items);
+                this.props.sendTotalUserCount(resp.data.totalCount);
+            } );
+    }
+
+    changePage = page => {
+        this.props.sendCurrentPage(page);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.usersOnPage}&page=${page}`)
+            .then( resp => {
+                this.props.sendUsers(resp.data.items);
+            } );
+    }
+
+    render() {
+        return <Users
+            users={this.props.users}
+            totalUserCount={this.props.totalUserCount}
+            usersOnPage={this.props.usersOnPage}
+            maxPaginationButtonOnPage={this.props.maxPaginationButtonOnPage}
+            currentPage={this.props.currentPage}
+            follow={this.props.follow}
+            unfollow={this.props.unfollow}
+            changePage={this.changePage}
+        />
+    }
+}
 
 let mapStateToProps = state => {
     return {
@@ -23,8 +58,8 @@ let mapDispatchToProps = dispatch => {
         unfollow: userId => dispatch( unfollowActionType(userId) ),
         sendUsers: newUsers => dispatch( sendUsersActionType(newUsers) ),
         sendTotalUserCount: totalUserCount => dispatch( sendTotalUserCountActionType(totalUserCount) ),
-        sendCurrentPage: page => dispatch( sendCurrentPageActionType(page) )
+        sendCurrentPage: page => dispatch( sendCurrentPageActionType(page) ),
     }
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersComponent);
